81e2a69fe45798694959fe5376729af798e31836
changhyeok <unixking@naver.com>
2023-07-28 14:24:36 +0900

    -

---
+++ level1/card_deck.py
@@ -0,0 +1,14 @@
+def solution(cards1, cards2, goal):
+    for i in goal:
+        if cards1 and i == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and i == cards2[0]:
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
+
+
+cards1 = ["i", "drink", "water"]	
+cards2 = ["want", "to"]
+goal = ["i", "want", "to", "drink", "water"]
