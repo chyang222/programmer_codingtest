ed4042499dd1cef9c47d6c4f83ca70654384e1f7
changhyeok <unixking@naver.com>
2023-07-28 14:04:05 +0900

    -

---
+++ level1/two_pw.py
@@ -0,0 +1,22 @@
+def solution(s, skip, index):
+    answer = ''
+    
+    for c in s:
+        i = ord(c)
+        j = index
+        while j > 0:
+            i += 1
+            if i > ord('z'):
+                i = ord('a')
+            if chr(i) in skip:
+                j += 1
+            j -= 1
+        answer += chr(i)
+    
+    return answer
+
+s = 'zzzzzz'
+skip = 'abcdefghijklmnopqrstuvwxy'
+index = 6
+
+solution(s, skip, index)
